e7b8e5af6cb2ccf0bb6c3120273ad8856da8e36c
arthurmiko <arthurmiko@gmail.com>
2016-07-08 18:42:06 +0300

    practice with shadows

---
+++ shadows_portfolio/scripts/common.js
@@ -0,0 +1,16 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  var simple = document.querySelector('.js_percentage');
+
+  lory(simple, {
+    infinite: 1,
+    slideSpeed: 1000
+  });
+});
+
+// var nextSlide = document.querySelector('.js_next');
+// var sliderTimer = setTimeout(function tick(){
+//   nextSlide.click();
+//   sliderTimer = setTimeout(tick, 3000);
+// }, 3000);
