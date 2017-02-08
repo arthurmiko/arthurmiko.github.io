4003cba96be931ecc35bc80b0aba1e4a8bda6850
arthurmiko <arthurmiko@gmail.com>
2017-02-08 12:48:13 +0300

    hue mix

--- test/main.js
+++ test/main.js
@@ -1,2 +1,31 @@
 'use strict';
 
+var bunchA = {
+  a: function(arg) {
+    console.log(arg)
+  },
+  // b: function() {...},
+  // c: function() {...},
+  
+  infoA: 'someA',
+  infoB: 5,
+  infoC: {
+    name: 'Alex'
+  }
+}
+
+var bunchB = {
+  a: function(arg) {
+    return arg;
+  },
+  // b: function() {...},
+  // c: function() {...},
+
+  infoA: 'someB',
+  infoB: 9,
+  infoC: {
+    name: 'Zick'
+  }
+}
+
+bunchA.a(bunchB.a(bunchB.infoA));
